a02e0c1800e59ea95f2c18caf8f49ab5f0d9d97b
ssiahoue <siahouesiaka1@gmail.com>
2023-02-19 11:37:02 +0000

    découverte des fonction anonymes

---
+++ anonymousfunc.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	w := func() {
+		println("je suis une fonction anonyme")
+	}
+	// appel de la fonction anonyme
+	w()
+
+	x, y := func(x, y int) (int, int) {
+		println("fonction anonyme avec retour de deux valeur dont ")
+		return x, y
+	}(10, 15)
+
+	fmt.Printf("la première valeur de retour est :%v \n \bla deuxième valeur de retour est :%v", x, y)
+
+}
